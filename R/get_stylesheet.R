6902b8bdb8b6913a271680539dfdba7728d83ade
Zhian N. Kamvar <zkamvar@gmail.com>
2020-06-04 10:01:23 -0700

    fix getter to change import element

--- R/get_stylesheet.R
+++ R/get_stylesheet.R
@@ -2,7 +2,7 @@ get_stylesheet <- function(sheet = "xml2md_gfm_kramdown.xsl") {
   tink <- system.file("extdata", "xml2md_gfm.xsl", package = "tinkr")
   ours <- system.file("stylesheets", sheet, package = "up2code")
   styl <- xml2::read_xml(ours)
-  href <- xml2::xml_child(styl, search = 1)
+  href <- xml2::xml_find_first(styl, ".//xsl:import")
   xml2::xml_set_attr(href, "href", tink)
   f <- fs::file_temp()
   xml2::write_xml(styl, f)
