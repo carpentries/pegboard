a28fa31691182043ef736c5a192e4ba766e5e4e1
Zhian N. Kamvar <zkamvar@gmail.com>
2020-09-23 16:15:08 -0700

    add label_divs() and questions methods

--- R/move_yaml.R
+++ R/move_yaml.R
@@ -1,10 +1,11 @@
 move_yaml <- function(yaml, body, what = "questions", dovetail = TRUE) {
   ln        <- xml_list_chunk(yaml, what, dovetail = dovetail)
   to_insert <- xml2::xml_children(ln)
-  xml2::xml_set_attr(to_insert, "dtag", what)
+  # xml2::xml_set_attr(to_insert, "dtag", what)
   where <- if (what == "keypoints") length(xml2::xml_children(body)) else 1L
   for (i in rev(to_insert)) {
     xml_slip_in(body, i, where)
   }
+  label_div_tags(body)
 }
 
