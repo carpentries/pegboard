3a944b0ca55ae164fa97fee986d90dd9aa333a4d
Zhian N. Kamvar <zkamvar@gmail.com>
2022-01-04 07:56:07 -0800

    escape all

--- R/get_stylesheet.R
+++ R/get_stylesheet.R
@@ -1,6 +1,6 @@
 get_stylesheet <- function(sheet = "xml2md_gfm_kramdown.xsl") {
   tink <- system.file("extdata", "xml2md_gfm.xsl", package = "tinkr")
-  tink <- xml2::url_escape(tink, reserved = "/")
+  tink <- xml2::url_escape(tink)
   ours <- system.file("stylesheets", sheet, package = "pegboard")
   styl <- readLines(ours)
   styl <- sub(
