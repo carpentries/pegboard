3d07b4d0e43541bf0f7e90086921c3829cfc4b53
Zhian N. Kamvar <zkamvar@gmail.com>
2022-01-04 07:48:45 -0800

    use xml2::url_escape()

--- R/get_stylesheet.R
+++ R/get_stylesheet.R
@@ -1,6 +1,6 @@
 get_stylesheet <- function(sheet = "xml2md_gfm_kramdown.xsl") {
   tink <- system.file("extdata", "xml2md_gfm.xsl", package = "tinkr")
-  tink <- gsub("\\s", "\\ ", fs::path_real(tink))
+  tink <- xml2::url_escape(tink, reserved = "/")
   ours <- system.file("stylesheets", sheet, package = "pegboard")
   styl <- readLines(ours)
   styl <- sub(
