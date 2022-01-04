18cabb8cde0f4933cb5fbb54a731be7ba6e90b6a
Zhian N. Kamvar <zkamvar@gmail.com>
2022-01-04 09:07:36 -0800

    actually escape

--- R/get_stylesheet.R
+++ R/get_stylesheet.R
@@ -1,6 +1,6 @@
 get_stylesheet <- function(sheet = "xml2md_gfm_kramdown.xsl") {
   tink <- system.file("extdata", "xml2md_gfm.xsl", package = "tinkr")
-  tink <- gsub("\\s", "\\ ", fs::path_real(tink))
+  tink <- gsub("\\s", "\\\\ ", fs::path_real(tink))
   ours <- system.file("stylesheets", sheet, package = "pegboard")
   styl <- readLines(ours)
   styl <- sub(
