bd85d98c67add44f54144d6ef9065a0f9d9cb9bb
Zhian N. Kamvar <zkamvar@gmail.com>
2022-01-05 11:37:03 -0800

    reserve colon for windows

--- R/get_stylesheet.R
+++ R/get_stylesheet.R
@@ -1,5 +1,5 @@
 get_stylesheet <- function(sheet = "xml2md_gfm_kramdown.xsl", import = system.file("extdata", "xml2md_gfm.xsl", package = "tinkr")) {
-  tink <- xml2::url_escape(import, reserved = "/")
+  tink <- xml2::url_escape(import, reserved = c("/", ":"))
   ours <- system.file("stylesheets", sheet, package = "pegboard")
   styl <- readLines(ours)
   styl <- sub(
