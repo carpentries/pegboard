7cfecdb84ac244aa99a1316fe0007ca49b24a85c
Zhian N. Kamvar <zkamvar@gmail.com>
2022-01-05 12:19:16 -0800

    THERE ARE FOUR BACKSLASHES

--- R/get_stylesheet.R
+++ R/get_stylesheet.R
@@ -1,7 +1,7 @@
 get_stylesheet <- function(sheet = "xml2md_gfm_kramdown.xsl", import = system.file("extdata", "xml2md_gfm.xsl", package = "tinkr")) {
   tink <- xml2::url_escape(import, reserved = c("/:\\"))
   if (.Platform$OS.type == "windows") {
-    tink <- gsub("/", "\\", tink)
+    tink <- gsub("/", "\\\\", tink)
   }
   ours <- system.file("stylesheets", sheet, package = "pegboard")
   styl <- readLines(ours)
