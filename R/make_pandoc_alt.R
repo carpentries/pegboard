c1c9a7be6a806b41c7f9f456e9ed76b189b2ee62
Zhian N. Kamvar <zkamvar@gmail.com>
2022-05-04 13:45:51 -0700

    add test and comments to make_pandoc_alt

--- R/make_pandoc_alt.R
+++ R/make_pandoc_alt.R
@@ -4,21 +4,39 @@
 #' @return the images, invisibly with a new alt attribute and text removed
 make_pandoc_alt <- function(images) {
   alt <- xml2::xml_text(images)
+  # There is reason to have alt="" in HTML to indicate a decorative image. 
+  # In the case for our lessons, this was never specified and also it's
+  # difficult to properly express alt="" in markdown syntax.
+  #
+  # Thus:
+  # ![text](img) // has alt text
+  # ![](img) // NO alt text
+  # ![ ](img) // decorative
+  has_alt <- alt != ""
   has_attrs <- xml2::xml_find_lgl(images, 
     "boolean(self::*[following-sibling::*[1][starts-with(text(), '{')]])")
-  to_append <- alt[has_attrs]
-  to_create <- alt[!has_attrs]
+
+  # Add alt text to images that already has attributes
+  to_append <- alt[has_attrs & has_alt]
   if (length(to_append)) {
-    nodes <- xml2::xml_find_first(images[has_attrs],
-      "self::*/following-sibling::*[1]")
+    imgs  <- images[has_attrs & has_alt]
+    nodes <- xml2::xml_find_first(imgs, "self::*/following-sibling::*[1]")
     txt <- xml2::xml_text(nodes)
     subbed <- purrr::map2_chr(to_append, txt, 
-      ~sub("[{][:]? ?", glue::glue("{alt=^shQuote(.x)$ ",
+      ~sub("[{][:]? ?", glue::glue("{alt=^shQuote(trimws(.x))$ ",
         .open = "^", .close = "$"), .y))
+    # set the postfix tag 
     xml2::xml_set_text(nodes, subbed)
+    # set the attribute
+    xml2::xml_set_attr(imgs, "alt", to_append)
+    # remove previous caption text
+    xml2::xml_remove(xml2::xml_children(imgs))
   }
+
+  # Create a new alt text attribute
+  to_create <- trimws(alt[!has_attrs & has_alt])
   if (length(to_create)) {
-    itc <- images[!has_attrs]
+    itc <- images[!has_attrs & has_alt]
     new_alt <- glue::glue("<text>{alt=^shQuote(to_create)$}</text>",
       .open = "^", .close = "$")
     new_nodes <- make_text_nodes(new_alt)
---
+++ tests/testthat/test-make_pandoc_alt.R
@@ -0,0 +1,23 @@
+
+test_that("make_pandoc_alt() converts alt text good", {
+
+  f <- textConnection(c("![has alt text](img1.png)", 
+      "",
+   "![](needs-alt.png)", 
+      "",
+   "![ ](decorative.png)",
+      "",
+   "![has alt text](img2.png){: .class}", ""))
+  body <- tinkr::to_xml(f)$body
+  ns <- tinkr::md_ns()
+  images <- xml2::xml_find_all(body, ".//md:image", ns = ns)
+  make_pandoc_alt(images)
+  # Alt text is expected only when nodes indicate that it is expected
+  expect_equal(xml2::xml_attr(images, "alt"), 
+    c("has alt text", NA, "", "has alt text"))
+  # captions are not present (because we did not previously have ways to 
+  # incorporate captions
+  expect_equal(xml2::xml_text(images),
+    rep("", 4))
+})
+
