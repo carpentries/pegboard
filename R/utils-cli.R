e45fc35ed3b66985a2baac6e0fdabada3efec502
Zhian N. Kamvar <zkamvar@gmail.com>
2022-05-25 16:30:28 -0700

    add custom class of message

--- R/utils-cli.R
+++ R/utils-cli.R
@@ -16,8 +16,42 @@ issue_warning <- function(msg, cli = has_cli(), ...) {
   if (cli) {
     cli::cli_alert_warning(msg)
   } else {
-    message(glue::glue("! {glue::glue(msg)}"))
+    pb_message(glue::glue("! {glue::glue(msg)}"))
   }
+  invisible()
+}
+
+#' Utility to make "pegboard" class of messages 
+#'
+#' This allows us to control the messages emitted _and_ continue to keep CLI as
+#' a suggested package.
+#'
+#' The vast majority of the code in this function is copied directly from the
+#' [message()] function.
+#'
+#' @inheritParams base::message
+#' @rdname cli_helpers
+#' @examples
+#' pb_message("hello")
+pb_message <- function (..., domain = NULL, appendLF = TRUE) {
+  # nocov start
+  cond <- if (...length() == 1L && inherits(..1, "condition")) {
+    if (nargs() > 1L)
+      warning("additional arguments ignored in message()")
+    ..1
+  } else {
+  # nocov end
+    msg <- .makeMessage(..., domain = domain, appendLF = appendLF)
+    structure(list(message = msg), 
+      class = c("pbMessage", "simpleMessage", "message", "condition"))
+  }
+  defaultHandler <- function(c) {
+    cat(conditionMessage(c), file = stderr(), sep = "")
+  }
+  withRestarts({
+    signalCondition(cond)
+    defaultHandler(cond)
+  }, muffleMessage = function() NULL)
   invisible()
 }
 
@@ -63,4 +97,43 @@ append_labels <- function(l, i = TRUE, e = "", cli = FALSE, f = "style_inverse")
   f <- if (cli) utils::getFromNamespace(f, "cli") else function(e) e
   l[i] <- paste(l[i], f(e))
   l
+}
+
+#' Swallow messages from the cli and pegboard packages
+#'
+#' @param expr an R expression.
+#' @param keep if `TRUE`, the messages are kept in a list. Defautls to `FALSE`
+#'   where cli message are discarded.
+#' @return if `keep = FALSE`, the output of `expr`, if `keep = TRUE`, a list
+#'   with the elements `val = expr` and `msg = <cliMessage>s`
+#' @rdname cli_helpers
+#' @examples
+#' pegboard:::message_muffler({
+#'   cli::cli_text("hello there! I'm staying in!")
+#'   pegboard:::pb_message("normal looking message that's not getting through")
+#'   message("this message makes it out!")
+#'   runif(1)
+#' })
+#' pegboard:::message_muffler({
+#'   cli::cli_text("hello there! I'm staying in!")
+#'   pegboard:::pb_message("normal looking message that's not getting through")
+#'   message("this message makes it out!")
+#'   runif(1)
+#' }, keep = TRUE)
+message_muffler <- function(expr, keep = FALSE) {
+  messages <- NULL
+  expr <- substitute(expr)
+  collector <- function(msg) {
+    messages <<- c(messages, list(msg))
+    invokeRestart("muffleMessage")
+  }
+  val <- withCallingHandlers(eval.parent(expr), 
+    cliMessage = collector, 
+    pbMessage = collector
+  )
+  if (keep) {
+    return(list(val = val, msg = messages))
+  } else {
+    return(val)
+  }
 }
