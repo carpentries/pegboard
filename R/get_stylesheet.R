f73de3cf77ee9c2f52cd999b36150e5fe5c8a7c9
Zhian N. Kamvar <zkamvar@gmail.com>
2022-08-15 10:01:56 -0700

    update for new tinkr

--- R/get_stylesheet.R
+++ R/get_stylesheet.R
@@ -1,4 +1,4 @@
-get_stylesheet <- function(sheet = "xml2md_gfm_kramdown.xsl", import = system.file("extdata", "xml2md_gfm.xsl", package = "tinkr")) {
+get_stylesheet <- function(sheet = "xml2md_gfm_kramdown.xsl", import = tinkr::stylesheet()) {
   tink <- xml2::url_escape(fs::path_real(import), reserved = c("/:\\"))
   ours <- system.file("stylesheets", sheet, package = "pegboard")
   styl <- readLines(ours)
