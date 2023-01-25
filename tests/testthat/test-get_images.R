11f90c811a256f2b4123bf6d375a833b8e86fa34
Zhian N. Kamvar <zkamvar@gmail.com>
2023-01-24 18:08:36 -0800

    update test with long alt

--- tests/testthat/test-get_images.R
+++ tests/testthat/test-get_images.R
@@ -5,11 +5,14 @@ test_that("markdown images will process alt text appropriately", {
     "![image2](b.png){alt='text\nalt' width='20%'}", 
     "![image3](c.png){ width='31%' alt='something long and proseful' style='border-radius: \"50%\"'}", 
     "![",
-    "image3 with a longer caption", 
-    "](c.png){ width='31%'",
+    "image4 with a longer caption", 
+    "](d.png){ width='31%'",
     "alt='attributes on separate lines'",
-    "style='border-radius: \"25%\"}'",
-    "![image4](d.png)", 
+    "style='border-radius: \"25%\"'}",
+    "![image5](e.png)", 
+    "![The {pegboard} package](pegboard.png){width='49%'",
+    "alt='The logo for the {pegboard} package, which",
+    "appears as a little workshop with a pegboard on the wall'}",
     "text")
   txt <- paste(imgs, collapse = "\n")
   
@@ -21,7 +24,8 @@ test_that("markdown images will process alt text appropriately", {
     "text alt", 
     "something long and proseful", 
     "attributes on separate lines",
-    NA_character_)
+    NA_character_,
+  "The logo for the {pegboard} package, which appears as a little workshop with a pegboard on the wall")
   expect_equal(xml2::xml_attr(images, "alt"), expected_alts)
 })
 
