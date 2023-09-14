49bdd9ac938f62489a781c1c1c5ecc58dc2107e2
Zhian N. Kamvar <zkamvar@gmail.com>
2023-09-14 11:24:14 -0700

    write find_children documentation

--- R/find_children.R
+++ R/find_children.R
@@ -3,31 +3,109 @@
 #' @description 
 #'  - `find_children()` returns the _immediate_ children for any given Episode
 #'    object. 
-#'  - `child_file_from_code_blocks()` extracts the `child` attribute from XML
-#'    `code_block` nodes
-#'  - `trace_children()` is used _after processing_ to trace the entire lineage
-#'     from a source parent episode.
+#'  - `trace_children()` is used _after processing_ in the context of a Lesson
+#'     to trace the entire lineage from a source parent episode.
 #'
-#' @param parent an [Episode] object
-#' @param nodes an `xml_nodeset` object containing `code_block` nodes that
-#'   may have `child` attributes pointing to the relative path of a file in the
-#'   lesson (see NOTE).
+#' @param parent an [Episode] or [tinkr::yarn] object (`trace_children()`
+#'   requires an `Episode` object).
 #' @param lsn a [Lesson] object that contains the `parent` and all its children.
+#' @return a character vector of the absolute paths to child files.
 #'
 #' @details 
 #'
+#' It is possible to define [child
+#' documents](https://bookdown.org/yihui/rmarkdown-cookbook/child-document.html)
+#' in \pkg{knitr} documents by using the `child` attribute in code chunks. For
+#' example, let's say we have a file called `episodes/parent.Rmd`:
 #' 
+#' ````markdown
+#' This content is from _a child document_:
+#' 
+#' ```{r child = "files/the-child.Rmd"}
+#' ```
+#' `````
+#'
+#' where `files/the-child.Rmd` is relative to `episodes/parent.Rmd`
+#'
+#' The `find_children()` function will extract the immediate children from
+#' a single [Episode] object (in this case, it will return
+#' `/path/to/episodes/files/the-child.Rmd`), but it will not detect any further
+#' descendants. To detect the entire lineage, the Episode must be read in the
+#' context of a Lesson (or processed with [load_children()]). 
+#'
+#' This function is used during Episode initialisation to populate the 
+#' `$children` element of the `Episode` object, which lists paths to the
+#' known children for further processing.
+#'
+#' ## Tracing full lineages
+#'
+#' It is possible for a child document to have further children defined:
+#'
+#' ````markdown
+#' This is the first child. The following content is from the grandchild:
+#'
+#' ```{r child = "the-grandchild.md"}
+#' ```
+#' ````
+#' 
+#' When an Episode is read _in the context of a Lesson_, the children are
+#' processed with [load_children()] so that each file with children will have a
+#' non-zero value of the `$children` element. We recurse through the `$children`
+#' element in the [Lesson] object to exhaust the search for the children files.
+#'
+#' The `trace_children()` will return the entire lineage for a given _parent_
+#' file. Which, in the case of the examples defined above would be:
+#' `/path/to/episodes/parent.Rmd`, `/path/to/episodes/files/the-child.Rmd`,
+#' and `/path/to/episodes/the-grandchild.md`. 
+#'
 #' ## NOTE
 #'
-#' For standard lessons, child files will appear relative to the parent file. 
-#' Usually, these child files will be in the `episodes/files` folder. Overview
-#' lessons are a little different. For overview lessons (in The Workbench,
-#' these are lessons which contain `overview: true` in config.yaml), the child
-#' files may point to `files/child.md`, but in reality, the child file is at
-#' the root of the lesson `../files/child.md`. 
+#' For standard lessons, child files are written relative to the parent file. 
+#' Usually, these child files will be in the `files` folder under their parent
+#' folder. Overview lessons are a little different. For overview lessons (in
+#' The Workbench, these are lessons which contain `overview: true` in
+#' config.yaml), the child files may point to `files/child.md`, but in reality,
+#' the child file is at the root of the lesson `../files/child.md`. We correct
+#' for this by first checking that the child files exist and if they don't
+#' defaulting to the top of the lesson. 
 #'
 #' @keywords internal
 #' @rdname find_children
+#' @examples
+#' # This example demonstrates a child document with another child document
+#' # nested inside. First, we demonstrate how `find_children()` only returns
+#' # the immediate children and then we demonstrate how the full lineage is
+#' # extracted in the Lesson object.
+#' #
+#' # `find_children()` --------------------------------------------------------
+#' ex <- lesson_fragment("sandpaper-fragment-with-child")
+#' 
+#' # The introduction has a single child file
+#' intro <- tinkr::yarn$new(fs::path(ex, "episodes", "intro.Rmd"))
+#' intro$head(21) # show the child file
+#' find_children(intro)
+#' # this is identical to the `$children` element of an Episode object
+#' ep <- Episode$new(fs::path(ex, "episodes", "intro.Rmd"))
+#' ep$children
+#' 
+#' # Loading the child file reveals another child
+#' child <- Episode$new(ep$children[[1]])
+#' child$children
+#' child$show()
+#' 
+#' # `trace_children()` -------------------------------------------------------
+#' # In the context of the lesson, we can find all the descendants
+#' lsn <- Lesson$new(ex, jekyll = FALSE)
+#' trace_children(ep, lsn)
+#' # This is the same as using the method of the same name in the Lesson object
+#' # using the path to the episode
+#' lsn$trace_lineage(ep$path)
+#' # show the children
+#' purrr::walk(lsn$children, function(ep) {
+#'     message("----", ep$path, "----")
+#'     ep$show()
+#'   }
+#' )
 find_children <- function(parent) {
   code_blocks <- get_code(parent$body, type = NULL, attr = NULL)
   children <- child_file_from_code_blocks(code_blocks)
@@ -61,6 +139,7 @@ child_file_from_code_blocks <- function(nodes) {
 
 # trace the lineage of a source file and return a recursive list of children
 # files. This assumes that the lesson has been set up to process children
+#' @rdname find_children
 trace_children <- function(parent, lsn) {
   if (parent$has_children) {
     children <- purrr::map(lsn$children[parent$children], trace_children, lsn)
