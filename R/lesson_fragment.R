8bbc257ba307bc0e5257469e22b08eda24278947
Zhian N. Kamvar <zkamvar@gmail.com>
2023-11-17 11:13:00 -0800

    fix typos

--- R/lesson_fragment.R
+++ R/lesson_fragment.R
@@ -10,7 +10,7 @@
 #'   - lesson-fragment
 #'   - rmd-lesson
 #'   - sandpaper-fragment
-#'   - sandpaper-fragment with children
+#'   - sandpaper-fragment with child
 #'
 #' @return a path to a lesson fragment whose contents are:
 #'   - `lesson-fragment` contains one `_episodes` directory with three files:
@@ -19,7 +19,7 @@
 #'     `01-test.Rmd`. 
 #'   - `sandpaper-fragment` contains a trimmed-down Workbench lesson that
 #'     has its R Markdown content pre-built
-#'   - `sandpaper-fragment-with-children` contains much of the same content as
+#'   - `sandpaper-fragment-with-child` contains much of the same content as
 #'   `sandpaper-fragment`, but the `episodes/index.Rmd` file references child
 #'   documents.
 #' @export
@@ -27,10 +27,10 @@
 #' lesson_fragment()
 #' lesson_fragment("rmd-lesson")
 #' lesson_fragment("sandpaper-fragment")
-#' lesson_fragment("sandpaper-fragment-with-children")
+#' lesson_fragment("sandpaper-fragment-with-child")
 lesson_fragment <- function(name = "lesson-fragment") {
   allowed <- c("lesson-fragment", "rmd-lesson", 
-    "sandpaper-fragment", "sandpaper-fragment-with-children")
+    "sandpaper-fragment", "sandpaper-fragment-with-child")
   name <- match.arg(name, allowed)
   return(system.file(name, package = "pegboard"))
 }
